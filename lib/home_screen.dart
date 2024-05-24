b5430e6a117c2cff928b898cebfa2a11b1464824
prajwal <prajwal77d@gmail.com>
2024-05-24 13:41:06 +0530

    first commit

---
+++ lib/home_screen.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xffF1E5D1),
+      appBar: AppBar(
+        title: const Text("Currency Converter"),
+        centerTitle: true,
+        backgroundColor: const Color(0xffF1E5D1),
+        elevation: 0.0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "0",
+              style: TextStyle(fontSize: 32),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            const TextField(
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              style: TextStyle(color: Colors.black),
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.monetization_on_outlined),
+                hintText: "Enter a amount here",
+                hintStyle: TextStyle(color: Colors.grey),
+                filled: true,
+                fillColor: Colors.white,
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(8),
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(8),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  foregroundColor: Colors.white,
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.horizontal(
+                      left: Radius.circular(8),
+                      right: Radius.circular(8),
+                    ),
+                  ),
+                  fixedSize: const Size(900, 50)),
+              child: const Text("Convert"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
